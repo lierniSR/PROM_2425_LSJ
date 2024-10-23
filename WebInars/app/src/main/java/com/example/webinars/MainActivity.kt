8bc970e564d5990f16f2c701ba7b4784d154f7a2
Lierni <lierni@gmail.com>
2024-10-23 13:17:10 +0200

    Variables y listas hecho

--- WebInars/app/src/main/java/com/example/webinars/MainActivity.kt
+++ WebInars/app/src/main/java/com/example/webinars/MainActivity.kt
@@ -7,14 +7,30 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var example:String //Para que se inizialize mas tarde, pero si no se asigna ningun valor app da error
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        test()
+        testSimp()
     }
+    //Metodos:
+    //Se puede asignar valor a la variable que se recoge en el metodo
+    //Fuera de los parentesis que objeto devuelve
+    fun test(saludo:String = "hola"){
+        //Mejor poner de que tipo es la val: no se puede cambiar valor
+        //Var se puede cambiar valor
+        //Listas;
+        //Asi no te va a dejar añadir mas elementos
+        val list = listOf<String>("val1", saludo)
+        //Con lista mutable si puedes añadir mas elementos
+        //Se pone en val porque no se crea otra lista, los cambios se hacen en mutableListOf no en listMutable, esta val va a apuntar siempre al mismo mutableListOf
+        val listMutable = mutableListOf<String>("val1", saludo)
+        listMutable.add("Pureba")
+        //Asignar al var example
+        example = "HolaLateInit"
+    }
+
+    //Metodo simplificado
+    fun testSimp(name:String = "desconocido") = "hola, $name"
 }
