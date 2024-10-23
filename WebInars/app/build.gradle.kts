e5d29ee05ef976a84a6e098f41d26390a83111d6
Lierni <lierni@gmail.com>
2024-10-23 12:58:49 +0200

    Parte del scroll inifito

--- WebInars/app/build.gradle.kts
+++ WebInars/app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    // https://mvnrepository.com/artifact/com.google.android.material/material
+    runtimeOnly("com.google.android.material:material:1.9.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
